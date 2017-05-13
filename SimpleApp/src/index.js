b2e0c8adb7dbeff010502ec61b7a70ebf3fcbbc1
Therk <therk@me.com>
2017-05-13 14:37:09 -0400

    Added debounce from lodash to delay executing the video search

--- SimpleApp/src/index.js
+++ SimpleApp/src/index.js
@@ -1,12 +1,13 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
+import _ from 'lodash';
 
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
 
-const API_KEY = 'AIzaSyAreMDiXrKC7G7knolClnQao-GBhTaMFc4';
+const API_KEY = '';
 
 class App extends Component {
   constructor(props) {
@@ -31,9 +32,12 @@ class App extends Component {
   }
 
   render(){
+
+    const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
+
     return (
       <div>
-      <SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
+      <SearchBar onSearchTermChange={term => videoSearch(term)}/>
     <VideoDetail video={this.state.selectedVideo} />
 
       <VideoList
