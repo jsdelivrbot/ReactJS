824f17d7b6e2017fe4ce667524545aca4fd94775
Therk <therk@me.com>
2017-05-20 12:45:56 -0400

    Initial React Redux app for Weather

---
+++ WeatherApp/src/containers/search_bar.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class SearBar extends Component {
+  render() {
+    return(
+      <form className="input-group">
+        <input></input>
+      </form>
+    );
+  }
+}
