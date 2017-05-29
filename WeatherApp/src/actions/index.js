4bee5700d958cdab27217010f5971680a05feec2
Therk <therk@me.com>
2017-05-29 15:16:56 -0400

    Added Weather List component and a weather reducer

--- WeatherApp/src/actions/index.js
+++ WeatherApp/src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = "e0ad7550142189ed7db0e5ed40fdbc8b";
-const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
---
+++ WeatherApp/src/containers/weather_list.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+
+class WeatherList extends Component {
+  render() {
+    return (
+      <table className="table table-hover">
+      <thead>
+      <tr>
+      <th>City</th>
+      <th>Temparture</th>
+      <th>Pressure</th>
+      <th>Humidity</th>
+      </tr>
+      </thead>
+      </table>
+    );
+  }
+}
+
+function mapStateToProps({ weather }) {
+  return { weather }; // ES6 {weather } === {weather: weather}
+}
+
+export default connect(mapStateToProps)(WeatherList);
