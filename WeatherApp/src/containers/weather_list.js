3006560b7709b91be860e19b43e95e37bb36bd8d
Therk <therk@me.com>
2017-07-02 08:57:23 -0400

    Fixed up WeatherList component to be a table and exported it

--- WeatherApp/src/containers/weather_list.js
+++ WeatherApp/src/containers/weather_list.js
@@ -5,21 +5,21 @@ class WeatherList extends Component {
   render() {
     return (
       <table className="table table-hover">
-      <thead>
-      <tr>
-      <th>City</th>
-      <th>Temparture</th>
-      <th>Pressure</th>
-      <th>Humidity</th>
-      </tr>
-      </thead>
+        <thead>
+          <tr>
+            <th>City</th>
+            <th>Temparture</th>
+            <th>Pressure</th>
+            <th>Humidity</th>
+          </tr>
+        </thead>
       </table>
     );
   }
 }
 
-function mapStateToProps({ weather }) {
-  return { weather }; // ES6 {weather } === {weather: weather}
+function mapStateToProps({weather}) {
+  return {weather}; // ES6 {weather } === {weather: weather}
 }
 
 export default connect(mapStateToProps)(WeatherList);
